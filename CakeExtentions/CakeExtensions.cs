da1755bb3d9b5a6828e45bb87c15d02f0b7993d2
Mousavi Hajar <Ha_Mousavi@iranet.net>
2018-08-27 11:42:14 +0430

    init

---
+++ CakeExtentions/CakeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Cake.Core;
+using Cake.Core.Annotations;
+using Newtonsoft.Json;
+using CakeExtentions;
+
+public static class CakeExtensions
+{
+    [CakeMethodAlias]
+    public static ConfigurationModel GetConfiguration(this ICakeContext context, string jsonPath)
+    {
+        if (System.IO.File.Exists(jsonPath) == false)
+            return null;
+
+        var json = System.IO.File.ReadAllText(jsonPath);
+        ConfigurationModel configuration = JsonConvert.DeserializeObject<ConfigurationModel>(json);
+        return configuration;
+    }
+}
---
+++ CakeExtentions/ConfigurationModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace CakeExtentions
+{
+    public enum Environment
+    {
+        [Description("محیط تستی")]
+        Test,
+        [Description("محیط عملیاتی")]
+        Operational
+    }
+
+    public class ProjectModel
+    {
+        public string Name { get; set; }
+        public bool IsEnabled { get; set; }
+    }
+
+    public class EnvironmentModel
+    {
+        public Environment Type { get; set; }
+        public bool IsEnabled { get; set; }
+        public List<ProjectModel> Projects { get; set; }
+    }
+
+    public class ConfigurationModel
+    {
+        public string SolutionPath { get; set; }
+        public List<EnvironmentModel> Environments { get; set; }
+
+    }
+}
